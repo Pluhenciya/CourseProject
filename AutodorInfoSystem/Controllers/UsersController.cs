706afbe5f268f2583294112df82d5598090af54d
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2024-11-27 22:32:43 +0300

    add user authorization

---
+++ AutodorInfoSystem/Controllers/UsersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutodorInfoSystem.Data;
+using AutodorInfoSystem.Models;
+using AutodorInfoSystem.Services;
+
+namespace AutodorInfoSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AutodorContext _context;
+        private readonly UserService _userService;
+        private readonly TokenService _tokenService;
+
+        public UsersController(AutodorContext context, UserService userService, TokenService tokenService)
+        {
+            _context = context;
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Login, Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var CreateUser = new User
+                {
+                    Login = user.Login,
+                    Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
+                };
+
+                _context.Users.Add(CreateUser);
+                await _context.SaveChangesAsync();
+                return Redirect("~/");
+            }
+            return Redirect("~/");
+        }
+
+        public IActionResult Login()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([Bind("Login, Password")] User user)
+        {
+
+            var userExist = _userService.UserVerify(user);
+
+            if (userExist == null)
+            {
+                TempData["ErrorMessage"] = "Неверный логин или пароль.";
+                return Redirect("~/");
+            }
+
+            Response.Cookies.Append("A", _tokenService.CreateToken(userExist));
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("A");
+
+            return Redirect("~/");
+        }
+    }
+}
