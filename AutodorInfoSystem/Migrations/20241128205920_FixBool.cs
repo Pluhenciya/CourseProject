ccb3cba20ddc813213b5f7bbb4bccf543f38238b
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2024-11-29 02:15:24 +0300

    add design layout and projects views

---
+++ AutodorInfoSystem/Migrations/20241128205920_FixBool.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutodorInfoSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id_task",
+                table: "workers_has_tasks",
+                newName: "tasks_id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "id_worker",
+                table: "workers_has_tasks",
+                newName: "workers_id_worker");
+
+            migrationBuilder.RenameColumn(
+                name: "id_task",
+                table: "materials_has_tasks",
+                newName: "tasks_id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "id_material",
+                table: "materials_has_tasks",
+                newName: "materials_id_material");
+
+            migrationBuilder.RenameColumn(
+                name: "id_task",
+                table: "equipment_has_tasks",
+                newName: "tasks_id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "id_equipment",
+                table: "equipment_has_tasks",
+                newName: "equipment_id_equipment");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "quantity",
+                table: "workers_has_tasks",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValueSql: "'1'");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "quantity",
+                table: "materials_has_tasks",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValueSql: "'1'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "tasks_id_task",
+                table: "workers_has_tasks",
+                newName: "id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "workers_id_worker",
+                table: "workers_has_tasks",
+                newName: "id_worker");
+
+            migrationBuilder.RenameColumn(
+                name: "tasks_id_task",
+                table: "materials_has_tasks",
+                newName: "id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "materials_id_material",
+                table: "materials_has_tasks",
+                newName: "id_material");
+
+            migrationBuilder.RenameColumn(
+                name: "tasks_id_task",
+                table: "equipment_has_tasks",
+                newName: "id_task");
+
+            migrationBuilder.RenameColumn(
+                name: "equipment_id_equipment",
+                table: "equipment_has_tasks",
+                newName: "id_equipment");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "quantity",
+                table: "workers_has_tasks",
+                type: "int",
+                nullable: false,
+                defaultValueSql: "'1'",
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "quantity",
+                table: "materials_has_tasks",
+                type: "int",
+                nullable: false,
+                defaultValueSql: "'1'",
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
