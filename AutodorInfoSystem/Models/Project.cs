5498177352fb9e3a882e6b09b2251de3e9401271
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2024-12-03 16:20:02 +0300

    fix start pages

--- AutodorInfoSystem/Models/Project.cs
+++ AutodorInfoSystem/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AutodorInfoSystem.Models;
 
@@ -7,6 +8,7 @@ public partial class Project
 {
     public int IdProject { get; set; }
 
+    [Required(ErrorMessage = "Поле Название обязательно")]
     public string Name { get; set; } = null!;
 
     public string? Description { get; set; } = null!;
