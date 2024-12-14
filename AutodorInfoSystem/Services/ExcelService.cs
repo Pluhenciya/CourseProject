a58af1628decb26ff3e1549465179143a44d4976
Pluhenciya <vanya.ponomarv.00@bk.ru>
2024-12-14 16:06:00 +0300

    add excel service

---
+++ AutodorInfoSystem/Services/ExcelService.cs
@@ -0,0 +1,28 @@
+﻿using AutodorInfoSystem.Models;
+using ClosedXML.Excel;
+
+namespace AutodorInfoSystem.Services
+{
+    public class ExcelService
+    {
+        public async Task<MemoryStream> GenerateProjectReportAsync(Project project)
+        {
+            var memoryStream = new MemoryStream();
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add(project.Name);
+                GenerateProjectWorksheet(worksheet, project);
+                await System.Threading.Tasks.Task.Run(() => workbook.SaveAs(memoryStream));
+            }
+
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+
+        private void GenerateProjectWorksheet(IXLWorksheet worksheet, Project project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
